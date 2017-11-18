a5c317bc62a39cca66b5d979e7283e3d47dfd94b
Hisham Mubarak <parengalhisham@gmail.com>
2017-11-18 20:23:57 +0530

    Create Leaderboard Component

--- app/src/components/Leaderboard.js
+++ app/src/components/Leaderboard.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Text, View, StyleSheet,  TouchableHighlight } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import axios from 'axios';
 
 export default class Leaderboard extends React.Component {
+
+    constructor(props) {
+        super(props);    
+        this.state = {
+            toppers: []
+        }
+    }
+    
+    componentWillMount () {
+        axios.post('http://192.168.1.101:8000/leaderboard', {
+        })
+        .then(function(response){
+            response.data.data.forEach(element => {
+                this.state.toppers.push(element)
+            });
+        })
+        .catch(function(error){
+            console.log("Error", error);
+        });
+    }
+
     render() {
         return(
             <View style={styles.container}>
                 <Text style={styles.headerText}>Leaderboard</Text>
+
+                <ScrollView>
+                    <Text>Topper Name</Text>
+                </ScrollView>
             </View>
         )
     }
